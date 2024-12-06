8a81704ee878027abbb6fd2fb63f20e2d0f81926
AyobamiLad <moriahberry2222@gmail.com>
2024-12-06 20:12:59 +0100

    initialization of project

---
+++ app/super-admin/advertisement-management/page.tsx
@@ -0,0 +1,7 @@
+// import React from 'react'
+
+export default function AdvertisementManagement() {
+    return (
+     <div>Advertisement Management</div>   
+    )
+}
---
+++ app/super-admin/financials/page.tsx
@@ -0,0 +1,7 @@
+// import React from 'react'
+
+export default function Financials() {
+    return (
+     <div>Financials</div>   
+    )
+}
